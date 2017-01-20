801c5e05eacf282ef4f0466fb66fbb8a23d9d4a5
Elias Mason <eliasjmason@gmail.com>
2017-01-19 16:27:48 -0800

    Switches to future-proof ready listener

--- modal-window.js
+++ modal-window.js
@@ -1,27 +1,27 @@
 /*
- 
+
  ============================================
  License for Application
  ============================================
- 
+
  This license is governed by United States copyright law, and with respect to matters
  of tort, contract, and other causes of action it is governed by North Carolina law,
  without regard to North Carolina choice of law provisions.  The forum for any dispute
  resolution shall be in Wake County, North Carolina.
- 
+
  Redistribution and use in source and binary forms, with or without modification, are
  permitted provided that the following conditions are met:
- 
+
  1. Redistributions of source code must retain the above copyright notice, this list
  of conditions and the following disclaimer.
- 
+
  2. Redistributions in binary form must reproduce the above copyright notice, this
  list of conditions and the following disclaimer in the documentation and/or other
  materials provided with the distribution.
- 
+
  3. The name of the author may not be used to endorse or promote products derived from
  this software without specific prior written permission.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
  AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE
@@ -31,7 +31,7 @@
  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+
  */
 
 // jQuery formatted selector to search for focusable items
@@ -40,7 +40,7 @@ var focusableElementsString = "a[href], area[href], input:not([disabled]), selec
 // store the item that has focus before opening the modal window
 var focusedElementBeforeModal;
 
-$(document).ready(function() {
+$(function() {
     jQuery('#startModal').click(function(e) {
         showModal($('#modal'));
     });
